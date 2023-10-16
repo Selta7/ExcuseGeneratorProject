ea402f1691b986008299f3c47af34f06020ce2e7
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-10-16 18:31:41 +0000

    Excuse generator for class use!

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let resultExcuse = () => {
+    let who = ["The nerd", "The professor", "My classmates", "Myself"];
+    let verb = ["ate", "licked", "pooped", "shot"];
+    let what = ["an arrow", "a turd", "a graphics card", "a mouse"];
+    let when = ["yesterday", "a month ago", " a while"];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let verbIndex = Math.floor(Math.random() * verb.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoIndex] +
+      " " +
+      verb[verbIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+    );
+  };
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = resultExcuse();
 };
